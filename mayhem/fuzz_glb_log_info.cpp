981f30073a6aecb61c4c3bb7677dc486f43e3421
Raj Shah <rnshah9@asu.edu>
2022-12-23 19:54:38 +0000

    Remove FDP

--- mayhem/fuzz_glb_log_info.cpp
+++ mayhem/fuzz_glb_log_info.cpp
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include <climits>
 
-#include "FuzzedDataProvider.h"
+#include <fuzzer/FuzzedDataProvider.h>
 #include "log.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     FuzzedDataProvider provider(data, size);
-    std::string str = provider.ConsumeRandomLengthString();
+    std::string str = provider.ConsumeRandomLengthString(1000);
     glb_log_info(str.c_str());
 
     return 0;
